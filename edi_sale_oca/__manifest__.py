camptocamp/edi

Add edi_sale_oca

edi_sale_oca/__manifest__.py
@@ -1,0 +1,19 @@
+# Copyright 2022 Camptocamp SA
+# License LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl).
+
+{
+    "name": "EDI Sales",
+    "summary": """
+        Configuration and special behaviors for EDI on sales.
+    """,
+    "version": "14.0.1.0.0",
+    "license": "LGPL-3",
+    "author": "Camptocamp,Odoo Community Association (OCA)",
+    "website": "https://github.com/OCA/edi",
+    "depends": ["sale", "edi_oca", "component_event"],
+    "data": [
+        "views/res_partner.xml",
+        "views/sale_order.xml",
+        "views/edi_exchange_record.xml",
+    ],
+}

DeineTuer-GmbH/OCA_edi-framework

edi_sale_oca/__manifest__.py
@@ -1,0 +1,21 @@
+# Copyright 2022 Camptocamp SA
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    "name": "EDI Sales",
+    "summary": """
+        Configuration and special behaviors for EDI on sales.
+    """,
+    "version": "14.0.1.0.0",
+    "development_status": "Alpha",
+    "license": "AGPL-3",
+    "author": "Camptocamp,Odoo Community Association (OCA)",
+    "maintainers": ["simahawk"],
+    "website": "https://github.com/OCA/edi",
+    "depends": ["sale", "edi_oca", "component_event"],
+    "data": [
+        "views/res_partner.xml",
+        "views/sale_order.xml",
+        "views/edi_exchange_record.xml",
+    ],
+}